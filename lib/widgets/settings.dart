fe6087f22d3cf41ec71ea77888fd61ffd6d31727
Miloszzz2 <97192271+Miloszzz2@users.noreply.github.com>
2024-05-07 19:47:42 +0200

    signout button

--- lib/widgets/settings.dart
+++ lib/widgets/settings.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -10,6 +12,26 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  Future<void> _signOut() async {
+    try {
+      await Supabase.instance.client.auth.signOut();
+    } on AuthException catch (error) {
+      SnackBar(
+        content: Text(error.message),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      );
+    } catch (error) {
+      SnackBar(
+        content: const Text('Unexpected error occurred'),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      );
+    } finally {
+      if (mounted) {
+        Navigator.of(context).pushReplacementNamed('/login');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -38,6 +60,13 @@ class _SettingsState extends State<Settings> {
               text: Text(Supabase.instance.client.auth.currentUser != null
                   ? "Logout"
                   : "Login"),
+              onPressed: () {
+                if (Supabase.instance.client.auth.currentUser != null) {
+                  _signOut;
+                } else {
+                  Navigator.of(context).pushReplacementNamed('/login');
+                }
+              },
             )
           ],
         ),
